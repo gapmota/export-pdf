6cb84150bdb2b25b4b651e55960e6fe962b9e2a6
mottafoka <guiap.mota@hotmail.com>
2019-06-03 22:04:19 -0300

    1.1.2 | Postman

--- exportpdf/src/main/java/com/project/exportpdf/controller/RestExport.java
+++ exportpdf/src/main/java/com/project/exportpdf/controller/RestExport.java
@@ -2,13 +2,9 @@ package com.project.exportpdf.controller;
 
 import java.util.Date;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import com.project.exportpdf.model.PDF;
 
 /**
@@ -16,24 +12,21 @@ import com.project.exportpdf.model.PDF;
  *
  */
 
-@RestController
-@RequestMapping("/exportpdf")
+
 public class RestExport {
 
-	private final static String url = "http://localhost:8080/exportpdf/";
-	
-	private RestTemplate restTemplate;
-
-	public RestExport() {
-		restTemplate = new RestTemplate();
-	}
-
 	@PostMapping
-	public void exportPDF(int animalID, String animalName, Date lostDate, String animalInfos, String animalType,
-			String animalImgPath, String animalSitePath) {
-		PDF pdf = new PDF(animalID, animalName, lostDate, animalInfos, animalType, animalImgPath, animalSitePath);
-		ResponseEntity<String> answer = this.restTemplate.postForEntity(url, pdf, String.class);
-
-		System.out.println(answer);
+	public void exportPDF(@RequestBody InfoPet pet) {
+		PDF pdf = new PDF(pet.animalID, pet.animalName, pet.lostDate, pet.animalInfos, pet.animalType, pet.animalImgPath, pet.animalSitePath);
 	}
+}
+
+class InfoPet {
+    int animalID;
+	String animalName;
+	 Date lostDate;
+	 String animalInfos;
+	 String animalType;
+	 String animalImgPath;
+	 String animalSitePath;
 }
